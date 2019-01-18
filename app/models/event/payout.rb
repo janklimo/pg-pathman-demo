b91c1b4e8dc2303e34652d2fc50f37d5b9454f3c
Jan Klimo <jan.klimo@gmail.com>
2019-01-18 17:17:00 +0700

    Add models

---
+++ app/models/event/payout.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Payout < Event
+end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Gumroad
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '*/')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
